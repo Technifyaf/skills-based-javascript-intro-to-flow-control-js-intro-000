9397ca6e1712f39204864e0ce509ccbe031095b5
Thato Rammoko <technifyaf@gmail.com>
2021-01-22 18:23:51 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -16,6 +16,16 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
+  if (age <= 12){
+    return "You are a kid"
+  }
+  else if (age >=19 ){
+    return "You are a grown up"
+  }
+
+  else {
+    return"You are a teenager"
+  }
 
 }
 
